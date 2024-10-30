7dfe3d9f1d5a5e53c8f286216d1b14d940f801db
arthurpnuness <arthurpessoanunes@gmail.com>
2024-10-29 21:28:25 -0300

    up

---
+++ ex001.py
@@ -0,0 +1,16 @@
+'''Faça um algoritmo que contenha duas funções:
+Função 1: exiba um Olá
+Função 2: exiba um Tchau
+'''
+
+def ola():
+    print('OLA')
+
+def tchau():
+    print('TCHAU')
+
+def main():
+    ola()
+    tchau()
+
+main()
---
+++ ex002.py
@@ -0,0 +1,18 @@
+'''Faça um algoritmo que receba um valor e apresente o dobro e o triplo do valor.
+Utilize duas funções para realizar os cálculos
+'''
+
+def dobro():
+    print("b")
+    dobro_valor = int(input('Digite um valor para calcularmos o dobro dele: '))
+    return dobro_valor * 2
+
+def triplo():
+    triplo_valor = int(input('Digite um valor para calcularmos o triplo dele: '))
+    return triplo_valor * 3
+
+
+print("a")
+valor = dobro()
+
+
